b8f4fe70f7df3277248b7afcf1054796046931f4
FuSe55 <kirillov.aleksander@gmail.com>
2017-05-29 16:52:56 +0600

    1

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mainpage-app',
+  template: `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                fuhapough
+            </div>
+            <div class="col-md-6">
+                66666666666666666666666666666666
+            </div>
+            <div class="col-md-3">
+                3333333333333
+            </div>
+        </div>
+        
+    </div>
+  
+  `,
+})
+export class AppComponent  { name = 'Angular'; }
